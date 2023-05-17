d113865ae212bf16e2ab6f68d8482d7467284fe1
Hayat <hayatu4islam@yahoo.com>
2023-05-17 12:43:56 +0100

    Fourth test - paper vs. scissors

--- index.test.js
+++ index.test.js
@@ -1,6 +1,7 @@
 function rps(left, right) {
     return left === "rock" || right === "paper" ? "left" : "right";
 }
+
 describe("rock, paper, scissors", () => {
     it("should say left wins for rock vs. scissors", () => {
         const left = "rock";
